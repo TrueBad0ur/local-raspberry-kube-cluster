020b866a75efac94ce0090beec36913d22b2ac59
TrueBad0ur <NSA@google.com>
2024-07-29 11:50:58 +0300

    Add k8dash, grafana

---
+++ configs/K8Dash/man.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+kubectl create serviceaccount k8dash-sa
+kubectl create clusterrolebinding k8dash-sa --clusterrole=cluster-admin --serviceaccount=default:k8dash-sa
+
+kubectl create token k8dash-sa
