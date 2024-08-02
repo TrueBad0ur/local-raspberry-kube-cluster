b028f70f2280c360149f9a2ee324918e64c66453
TrueBad0ur <pieceofcakecupofcoffee@gmail.com>
2024-08-02 15:04:19 +0300

    Add nexus info

--- configs/nexus/install.sh
+++ configs/nexus/install.sh
@@ -1,1 +1,6 @@
 helm upgrade --install --namespace nexus --values ./values.yml nexus stevehipwell/nexus3
+
+in nexus: apt proxy
+Distribution: bookworm
+Remote storage: http://deb.debian.org/debian
+sources.list: deb http://nexus-nexus3.nexus.svc:8081/repository/apt-packages/ bookworm main
