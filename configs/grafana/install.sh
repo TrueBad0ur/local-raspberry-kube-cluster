5598a7340f05345140a68514c4346e945caa82f7
TrueBad0ur <pieceofcakecupofcoffee@gmail.com>
2024-08-11 21:58:15 +0300

    tls for grafana

--- configs/grafana/install.sh
+++ configs/grafana/install.sh
@@ -1,7 +1,9 @@
 helm repo add grafana https://grafana.github.io/helm-charts
+kubectl apply -f ./grafana/certificate.yml
 helm install -f ./grafana/values.yml grafana grafana/grafana -n monitoring
 
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
+kubectl apply -f ./prometheus/certificate.yml
 helm install -f ./prometheus/values.yml prometheus prometheus-community/prometheus -n monitoring
 
 prometheus server url - http://prometheus-server.monitoring.svc
