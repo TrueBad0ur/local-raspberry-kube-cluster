3796bb2d46f89cd0a7276ad40161a730c9ec4cbb
TrueBad0ur <pieceofcakecupofcoffee@gmail.com>
2024-12-02 13:19:02 -0500

    Info

--- configs/longhorn/install.sh
+++ configs/longhorn/install.sh
@@ -1,2 +1,4 @@
 helm repo add longhorn https://charts.longhorn.io
 helm install longhorn longhorn/longhorn --namespace longhorn-system -f values.yml
+
+mb you should patch and enable allowVolumeExpansion
