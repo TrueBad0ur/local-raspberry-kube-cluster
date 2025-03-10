84693a7d1f742ed2bd6292e88821f75e90a0d12b
TrueBad0ur <pieceofcakecupofcoffee@gmail.com>
2025-03-10 03:04:11 +0300

    Change rancher

--- configs/rancher/install.sh
+++ configs/rancher/install.sh
@@ -1,11 +1,5 @@
 helm repo add rancher-stable https://releases.rancher.com/server-charts/stable
 
-kubectl create namespace cattle-system
-
-helml install rancher rancher-stable/rancher \
-  --namespace cattle-system \
-  --set hostname=rancher.kubehomelab.space \
-  --set ingress.tls.source=letsEncrypt \
-  --set letsEncrypt.email=myemail@gmail.com
+helm install rancher rancher-stable/rancher --version 2.10.3 --namespace cattle-system --create-namespace -f values.yml
 
 k get secret --namespace cattle-system bootstrap-secret -o go-template='{{.data.bootstrapPassword|base64decode}}{{"\n"}}'
