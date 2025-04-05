c44ea5a9eb5ef2e3d592c0e2b6ea3c42821c2a6e
TrueBad0ur <pieceofcakecupofcoffee@gmail.com>
2025-04-06 02:14:28 +0300

    Add thelounge, ToDo: create helm chart for it

---
+++ configs/thelounge/deploy.sh
@@ -0,0 +1,13 @@
+#!/bin/zsh
+
+source ~/.zshrc
+
+k apply -f ns.yml
+
+k create secret generic user-config -n irc --from-file=truebad0ur.json
+
+k apply -f configmap.yml
+
+k apply -f deploy.yml
+
+k apply -f ingress.yml
