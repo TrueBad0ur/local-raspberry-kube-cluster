f5f677871d25d38360fc8c679749fa235f5a16f0
TrueBad0ur <pieceofcakecupofcoffee@gmail.com>
2025-04-07 20:52:03 +0300

    Started updating likec4 schemes

--- configs/likeC4/devel/build.sh
+++ configs/likeC4/devel/build.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-cd ..
-
-docker build -t likec4 -f devel/Dockerfile . && docker run --rm -p 8080:80 likec4:latest &
+docker build -t likec4 . && docker run --rm -p 8080:80 likec4:latest
 open -a "Firefox Developer Edition" 'http://localhost:8080'
