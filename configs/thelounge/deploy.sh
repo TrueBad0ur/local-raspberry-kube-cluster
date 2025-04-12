9e5c3f06c90c9534e6ef70af413e87924815facd
TrueBad0ur <pieceofcakecupofcoffee@gmail.com>
2025-04-12 14:48:44 +0300

    Helm chart for the-lounge

--- configs/thelounge/deploy.sh
+++ configs/thelounge/deploy.sh
@@ -11,3 +11,9 @@ k apply -f configmap.yml
 k apply -f deploy.yml
 
 k apply -f ingress.yml
+
+-----------------
+
+k create secret generic user-config -n irc --from-file=truebad0ur.json
+
+deploy with argo-cd
