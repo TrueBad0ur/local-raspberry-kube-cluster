fe6effb6ff6ede08f31e4f2c0033b1f345ccc6cd
TrueBad0ur <pieceofcakecupofcoffee@gmail.com>
2025-09-20 16:38:44 -0400

    Add gateway-api https example

--- configs/cert-manager/commands.sh
+++ configs/cert-manager/commands.sh
@@ -4,5 +4,8 @@ helm install \
   cert-manager jetstack/cert-manager \
   --namespace cert-manager \
   --create-namespace \
-  --version v1.15.2 \
-  --set crds.enabled=true
+  --version v1.18.2 \
+  --set crds.enabled=true \
+  --set config.apiVersion="controller.config.cert-manager.io/v1alpha1" \
+  --set config.kind="ControllerConfiguration" \
+  --set config.enableGatewayAPI=true
